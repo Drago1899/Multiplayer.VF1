8055c62c3111e4276fec83f21d29a86cc8f16ec8
Drago1899 <119632301+Drago1899@users.noreply.github.com>
2024-09-09 16:25:15 -0600

    Animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,29 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] int m_speed;
     Rigidbody2D m_rb2D;
     Vector2 m_movement;
+    public Animator animator;
+
+    PhotonView m_pV;
 
     // Start is called before the first frame update
     void Start()
     {
+        m_pV = GetComponent<PhotonView>();
         m_rb2D = GetComponent<Rigidbody2D>();
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float m_movementX = Input.GetAxis("Horizontal");
-        float m_movementY = Input.GetAxis("Vertical");
-        m_movement = new Vector2(m_movementY, m_movementX).normalized;
+        m_movement.x = Input.GetAxisRaw("Horizontal");
+        m_movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", m_movement.x);
+        animator.SetFloat("Vertical", m_movement.y);
+        animator.SetFloat("Speed", m_movement.sqrMagnitude);
+
+        m_rb2D.MovePosition(m_rb2D.position + m_movement * m_speed * Time.deltaTime);
+
     }
 
-    private void FixedUpdate() {
-        m_rb2D.MovePosition(m_rb2D.position + m_movement * m_speed * Time.deltaTime);
-    }
 }
+
