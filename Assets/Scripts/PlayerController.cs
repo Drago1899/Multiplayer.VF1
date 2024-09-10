7d9af64757a6fac140f77fb23ece00744055de1e
Drago1899 <119632301+Drago1899@users.noreply.github.com>
2024-09-10 09:28:37 -0600

    Coingo

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.SocialPlatforms.Impl;
 
 
 public class PlayerController : MonoBehaviour
@@ -10,6 +11,7 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D m_rb2D;
     Vector2 m_movement;
     public Animator animator;
+    [SerializeField] public int Score;
 
     PhotonView m_pV;
 
@@ -24,16 +26,30 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        m_movement.x = Input.GetAxisRaw("Horizontal");
-        m_movement.y = Input.GetAxisRaw("Vertical");
+        if (m_pV.IsMine)
+        {
+            m_movement.x = Input.GetAxisRaw("Horizontal");
+            m_movement.y = Input.GetAxisRaw("Vertical");
 
-        animator.SetFloat("Horizontal", m_movement.x);
-        animator.SetFloat("Vertical", m_movement.y);
-        animator.SetFloat("Speed", m_movement.sqrMagnitude);
+            animator.SetFloat("Horizontal", m_movement.x);
+            animator.SetFloat("Vertical", m_movement.y);
+            animator.SetFloat("Vel", m_movement.sqrMagnitude);
 
-        m_rb2D.MovePosition(m_rb2D.position + m_movement * m_speed * Time.deltaTime);
-
+            m_rb2D.MovePosition(m_rb2D.position + m_movement * m_speed * Time.deltaTime);
+        }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Diamante"))
+        {
+            Score++;
+            Destroy(collision.gameObject);
+
+        }
+        
+    }
+
 
 }
 
